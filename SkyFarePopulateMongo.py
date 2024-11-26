395935c260c3e0fd4caa46dedaa0925985c3a47b
Sebastian Parecattil <sdparecattil@gmail.com>
2024-11-26 01:26:09 -0600

    Duplicate error

---
+++ SkyFarePopulateMongo.py
@@ -0,0 +1,109 @@
+from pymongo import MongoClient
+import redis
+import re #regular expressions
+import json
+#keep track of how many pokemon we have processed
+processedpokemon = 0
+#The name of the mongo database
+mongoDBDatabase = 'SkyFare'
+mongoCollection = 'flightroutes'
+mongoUrl = "mongodb://localhost:27017/SkyFare"
+#The size of document upload batches
+batchSize = 50
+#database clients
+redisClient = redis.Redis(port=6379, decode_responses=True)
+## A helper function that builds a good mongoDB key
+## @param string the unicode string being keyified
+def mongoKeyify(string):
+    # remove bad chars, and disallow starting with an underscore
+    # string = re.sub("[\t \?\#\\\-\+\.\,'\"()*&!\/]+", '_', string)
+    s = re.sub(r"[-()\"#/@;:<>{}`+=~|.!?,\ ]", "_", string.lower())
+    return re.sub("^_+", '', s)
+## Insert documents into mongoDB in bulk.
+## @param documents The documents to store
+## @param count The number of documents being inserted.
+def saveDocs(documents, count):
+    with MongoClient(mongoUrl) as client:
+        dbo = client[mongoDBDatabase]
+        res = dbo[mongoCollection].insert_many(documents)
+        print("Number of documents inserted: " + str(len(res.inserted_ids)))
+
+
+def populateRoutes():
+    readRoutes = 0
+    routeBatch = []
+    for flightKey in redisClient.keys('flight:*'):
+        flightID = flightKey[7:28]
+
+        year = redisClient.hget("flight:" + flightID + ":info", "year")
+        quarter = redisClient.hget("flight:" + flightID + ":info", "quarter")
+        averageFare = redisClient.hget("flight:" + flightID + ":info", "averageFare")
+        nsmiles = redisClient.hget("flight:" + flightID + ":info", "nsmiles")
+
+        originAirport = redisClient.hget("flight:" + flightID + ":origin", "airport")
+        originCity = redisClient.hget("flight:" + flightID + ":origin", "city")
+        originGeocode = redisClient.hget("flight:" + flightID + ":origin", "geocode")
+        print(originGeocode +"----")
+        originLat = ""
+        originLon = ""
+        if (originGeocode != "" and "\n" in originGeocode):
+            originLat, originLon = originGeocode.split("\n")[1].strip("()").split(", ")
+        else:
+            print("skip orig")
+
+        destinationAirport = redisClient.hget("flight:" + flightID + ":destination", "airport")
+        destinationCity = redisClient.hget("flight:" + flightID + ":destination", "city")
+        destinationGeocode = redisClient.hget("flight:" + flightID + ":destination", "geocode")
+        destinationLat = ""
+        destinationLon = ""
+        if (destinationGeocode != "" and "\n" in destinationGeocode):
+            destinationLat, destinationLon = destinationGeocode.split("\n")[1].strip("()").split(", ")
+        else:
+            print("skip dest")
+
+        largestCarrierName = redisClient.hget("flight:" + flightID + ":largestCarrier", "name")
+        largestCarrierFare = redisClient.hget("flight:" + flightID + ":largestCarrier", "fare")
+
+        lowestCarrierName = redisClient.hget("flight:" + flightID + ":lowestCarrier", "name")
+        lowestCarrierFare = redisClient.hget("flight:" + flightID + ":lowestCarrier", "fare")
+
+        routeBatch.append({
+            "_id": mongoKeyify(flightID),
+            "year": year,
+            "quarter": quarter,
+            "origin": {
+                "airport": originAirport,
+                "city": originCity,
+                "latitude": originLat,
+                "longitude": originLon
+            },
+            "destination": {
+                "airport": destinationAirport,
+                "city": destinationCity,
+                "latitude": destinationLat,
+                "longitude": destinationLon
+            },
+            "largestCarrier": {
+                "name": largestCarrierName,
+                "fare": largestCarrierFare
+            },
+            "lowestCarrier": {
+                "name": lowestCarrierName,
+                "fare": lowestCarrierFare
+            },
+            "averageFare": averageFare,
+            "nsmiles": nsmiles
+        })
+
+        readRoutes += 1
+        if (len(routeBatch) >= batchSize):
+            saveDocs(routeBatch, len(routeBatch))
+            routeBatch = []
+        if (readRoutes % 100 == 0):
+            print ('Routes Loaded: ' + str(readRoutes))
+    if (len(routeBatch) > 0):
+        saveDocs(routeBatch, len(routeBatch))
+    print ('Routes Loaded: ' + str(readRoutes))
+
+populateRoutes()
+redisClient.quit()
