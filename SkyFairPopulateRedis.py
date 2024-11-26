8147cb9171e262605caad6fbb8eaa58bf4a0f2db
Sebastian Parecattil <sdparecattil@gmail.com>
2024-11-25 21:06:30 -0600

    Wrong code was uploaded, reuploading

--- SkyFairPopulateRedis.py
+++ SkyFairPopulateRedis.py
@@ -14,24 +14,54 @@ def populateRedis():
     with open(csvFilename, encoding="utf8") as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for data in csv_reader:
-            species = data['species']
-            pokemon = data['name']
-            pokemonObj = {
-            "pokedexNumber": data['pokedex_number'],
-            "pokemon": data['name'],
-            "height_m": data['height_m'],
-            "weight_kg": data['weight_kg']
+            #year = data['Year']
+            #quarter = data['quarter']
+            #cityOne = data['city1']
+            #cityTwo = data['city2']
+            #airportOne = data['airport_1']
+            #airportTwo = data['airport2']
+            #nsmiles = data['nsmiles']
+            #averageFare = data['fare']
+            #largestCarrier = data['carrier_lg']
+            #largestFare = data['fare_lg']
+            #lowestCarrier = data['carrier_low']
+            #lowestFare = data['fare_low']
+            #geocodeOne = data['Geocoded_City1']
+            #geocodeTwo = data['Geocoded_City2']
+
+            year = data['Year']
+            quarter = data['quarter']
+
+            origin = {
+                "airport": data['airport_1'],
+                "city": data['city1'],
+                "geocode": data['Geocoded_City1']
             }
+
+            destination = {
+                "airport": data['airport2'],
+                "city": data['city2'],
+                "geocode": data['Geocoded_City2']
+            }
+
+            largestCarrier = {
+                "name": data['carrier_lg'],
+                "fare": data['fare_lg']
+            }
+
+            lowestCarrier = {
+                "name": data['carrier_low'],
+                "fare": data['fare_low']
+            }
+
+            averageFare = data['fare']
+            nsmiles = data['nsmiles']
+
             line_count += 1
             if line_count%100 == 0:
                 print(f'Processed {line_count} lines.')
             redisClient.sadd('species:' + species, json.dumps(pokemonObj))
-            type_1 = data['type_1']
-            type_2 = data['type_2']
-            if type_1 != '':
-                redisClient.sadd("pokemon:"+species+":"+pokemon, type_1);
-            if type_2 != '':
-                redisClient.sadd("pokemon:"+species+":"+pokemon, type_2);
+            
     print(f'Processed {line_count} lines.')
 populateRedis();
 redisClient.quit();
